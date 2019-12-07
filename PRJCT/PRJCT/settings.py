b9c0277f2abdc64040d1fa993c8b3117668d2bd1
cheeruption <cheeruption@gmail.com>
2019-12-07 21:38:39 +0300

    pSQL credentials changes

--- PRJCT/PRJCT/settings.py
+++ PRJCT/PRJCT/settings.py
@@ -101,9 +101,9 @@ WSGI_APPLICATION = 'PRJCT.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'magicdb', #POSTGRES IS CASE INSENSITIVE!
-        'USER': 'magic_admin',
-        'PASSWORD': '12345',
+        'NAME': 'magicproject', #POSTGRES IS CASE INSENSITIVE!
+        'USER': 'cheeruption',
+        'PASSWORD': 'whatislove',
         'HOST': 'localhost',
         'PORT': '',
     }
