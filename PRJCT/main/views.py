628fb5882f51b1b61b4f2ea505574a2019f10a1c
cheeruption <cheeruption@gmail.com>
2019-12-10 14:56:51 +0300

    added card trasnlation funstionality

--- PRJCT/main/views.py
+++ PRJCT/main/views.py
@@ -25,5 +25,32 @@ def search(request):
     if request.method == "POST": #если запрос к странице содержит ПОСТ запрос - идем по данному пути
         form = SearchForm(request.POST) #вытаскиваем объект формы
         if form.is_valid():
-            context = {'card':form.cleaned_data.get('cardname')}
+            context = form.cleaned_data.get('cardname').replace('','+')
+            search_upd = f'https://api.scryfall.com/cards/named?fuzzy={context}'
+            r = requests.get(search_upd)
+            api_respond = r.json()
+            context = api_respond.get('name')
+
     return render(request, "main/prices.html", {'card':context, 'form':form})
+
+# поиск картинки по названию + перевод карты с ру на англ
+# SEARCHCARD = 'Fanat Fire'.replace(' ','+')
+# SEARCHCARD = input('what card? ').replace(' ','+')
+# search_upd = f'https://api.scryfall.com/cards/named?fuzzy={SEARCHCARD}'
+# r = requests.get(search_upd)
+# api_respond = r.json()
+
+
+# print(api_respond)
+
+# print(api_respond['image_uris'])
+
+# request_image = api_respond['image_uris']['normal']
+# print(request_image)
+# r = requests.get(request_image, stream = True)
+# if r.status_code == 200:
+#     with open('test.jpg', 'wb') as f:
+#         r.raw.decode_content = True
+#         shutil.copyfileobj(r.raw, f)
+# else:
+#     print('smth went wrong')
