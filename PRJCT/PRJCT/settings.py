16f48cfa4547df7ddc4de5865f8227c6c97f5ba6
cheeruption <cheeruption@gmail.com>
2020-01-05 00:30:36 +0300

    changed channels host in settings

--- PRJCT/PRJCT/settings.py
+++ PRJCT/PRJCT/settings.py
@@ -99,7 +99,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('134.209.225.140', 6379)],
         },
     },
 }
